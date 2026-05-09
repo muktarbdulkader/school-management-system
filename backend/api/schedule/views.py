70298c1b940d1cf3537e5f5010a8cba9325111dc
muktar abdulkader <muktarabdulkader35@gmail.com>
2026-05-09 10:57:03 +0300

    fix the use

--- backend/api/schedule/views.py
+++ backend/api/schedule/views.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets, status
+from rest_framework import viewsets, status, serializers
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.exceptions import PermissionDenied
 from rest_framework.response import Response
